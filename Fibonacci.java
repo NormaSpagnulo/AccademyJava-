ed9ee78744dbdc68248d2ff33c2c3448fa4063ab
NormaSpagnulo <125296943+NormaSpagnulo@users.noreply.github.com>
2023-02-20 12:49:02 +0100

    Secondo Esercizio - Fibonacci
    Aggiunta del controllo del termine pari

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,25 +3,32 @@ public class Fibonacci {
 
     public static void main(String[] args){
 
+        // creo lo scanner per l'inserimento dell'utente 
         Scanner ins = new Scanner(System.in);
         int ins_utente;
         System.out.println("Inserisci il numero da cui iniziare");
         ins_utente = ins.nextInt();
 
+        //inizializzo primo e secondo termine
 
          int primo_termine = 0, secondo_termine = 1;
 
         System.out.println("La serie di Fibonacci è:");
 
         for (int i = 1; i <= ins_utente; i++){
-            System.out.println(primo_termine + ", ");
+           // System.out.println(primo_termine + ", ");
 
-            //prossimo termine
             int prossimo_termine = primo_termine + secondo_termine;
             primo_termine = secondo_termine;
             secondo_termine = prossimo_termine;
+
+            //vedo se il prossimo termine è pari
+                if (prossimo_termine %2 == 0) 
+                    System.out.println("Il numero:" + prossimo_termine + " è pari");   
+
+            }
         }
 
     }
     
-}
+
